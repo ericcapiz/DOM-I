9df8f6ef16eba27087326609cdc2de0d7b23baab
eric capiz <ericcapiz@gmail.com>
2020-07-12 20:56:48 -0500

    small fixes

--- js/index.js
+++ js/index.js
@@ -42,14 +42,10 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 let nav = document.querySelectorAll('a');
+for (let i=0; i<nav.length; i++){
+  nav[i].textContent=siteContent ['nav'][`nav-item-${i+1}`];
+}
 
-
-nav[0].textContent=siteContent ['nav']['nav-item-1'];
-nav[1].textContent=siteContent ['nav']['nav-item-2'];
-nav[2].textContent=siteContent ['nav']['nav-item-3'];
-nav[3].textContent=siteContent ['nav']['nav-item-4'];
-nav[4].textContent=siteContent ['nav']['nav-item-5'];
-nav[5].textContent=siteContent ['nav']['nav-item-6'];
 
 nav.forEach(a => {
   a.style.color = "green";
